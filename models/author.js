bd354986993168060545e593359edd8ffe7d5d9b
Krzysztof9911 <k.wiesniakowski@student.utwente.nl>
2021-08-06 14:58:34 +0200

    Show/Edit/Update/Delete Routes for Author

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Recipe = require('./recipe')
 const authorSchema = new mongoose.Schema({
     name:{
         type: String,
@@ -6,4 +7,15 @@ const authorSchema = new mongoose.Schema({
     }
 })
 
+authorSchema.pre('remove', function(next) {
+    Recipe.find({author: this.id}, (err, recipies) => {
+        if(err){
+            next(err)
+        } else if (recipies.length > 0) {
+            next (new Error('This author has still recipies'))
+        }else{
+            next()
+        }
+    })
+})
 module.exports = mongoose.model('Author', authorSchema)
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const  indexRouter = require('./routes/index')
 const  authorRouter = require('./routes/authors')
@@ -20,6 +21,7 @@ app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 app.use(bodyParser.json())
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 
 const mongoose = require('mongoose')
