5679a729911c5e201e4bde49e440a027c15b7bb0
Krzysztof9911 <k.wiesniakowski@student.utwente.nl>
2021-08-09 20:50:30 +0200

    Finished styling, upgrading loading the webiste using JavaScript. Project finished

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,28 @@
-FilePond.registerPlugin(
+const rootStyles = window.getComputedStyle(document.documentElement)
+
+if(rootStyles.getPropertyValue ('--recipe-photo-width-large') != null){
+  ready()
+} else{
+  document.getElementById('main/css').addEventListener('load', ready)
+}
+
+function ready(){
+  const photoWidth = rootStyles.getPropertyValue('--recipe-photo-width-large')
+  const photoAspectRatio = rootStyles.getPropertyValue('--recipe-photo-aspect-ratio')
+  const photoHeight = photoWidth/photoAspectRatio;
+
+  FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
     FilePondPluginFileEncode,
   )
   
   FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
+    stylePanelAspectRatio: photoAspectRatio,
+    imageResizeTargetWidth: photoWidth,
+    imageResizeTargetHeight: photoHeight
   })
   
   FilePond.parse(document.body);
+}
+
